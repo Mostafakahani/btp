c425af45df5526907f4026a0b2abd9e1bc810926
Mostafa <kahanimostafa83@gmail.com>
2023-11-11 21:44:58 +0330

    ProductList added

---
+++ src/pages/Products/ProductList.js
@@ -0,0 +1,176 @@
+const Products = [
+    {
+        id: 1,
+        data: [
+            "#254",
+            {
+                img_Cover: '/ProductsImages/inPage/0101.png',
+                img_InPage: '/ProductsImages/inPage/01.png',
+                name_Persian: 'آسکوربیک اسید (ویتامین C)',
+                name_English: 'Ascorbic Acid',
+                category: 'اسید',
+                weight: 25,
+                url: '/images/avatar.png',
+                packing: 25,
+                typeOfUse: 'گرید خوراکی',
+                country: 'چین',
+                lable: 'Zibo',
+            },
+        ],
+        inPage: [
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+
+        ]
+    },
+    {
+        id: 2,
+        data: [
+            "#254",
+            {
+                img_Cover: '/ProductsImages/inPage/01.png',
+                img_InPage: '/ProductsImages/inPage/01.png',
+                name_Persian: 'آسکوربیک اسید (ویتامین C)',
+                name_English: 'Ascorbic Acid',
+                category: 'اسید',
+                weight: 25,
+                url: '/images/avatar.png',
+                packing: 25,
+                typeOfUse: 'گرید خوراکی',
+                country: 'چین',
+                lable: 'Zibo',
+            },
+        ],
+        inPage: [
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+
+        ]
+    },
+    {
+        id: 3,
+        data: [
+            "#254",
+            {
+                img_Cover: '/ProductsImages/inPage/01.png',
+                img_InPage: '/ProductsImages/inPage/01.png',
+                name_Persian: 'آسکوربیک اسید (ویتامین C)',
+                name_English: 'Ascorbic Acid',
+                category: 'اسید',
+                weight: 25,
+                url: '/images/avatar.png',
+                packing: 25,
+                typeOfUse: 'گرید خوراکی',
+                country: 'چین',
+                lable: 'Zibo',
+            },
+        ],
+        inPage: [
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+
+        ]
+    },
+    {
+        id: 4,
+        data: [
+            "#254",
+            {
+                img_Cover: '/ProductsImages/inPage/01.png',
+                img_InPage: '/ProductsImages/inPage/01.png',
+                name_Persian: 'آسکوربیک اسید (ویتامین C)',
+                name_English: 'Ascorbic Acid',
+                category: 'اسید',
+                weight: 25,
+                url: '/images/avatar.png',
+                packing: 25,
+                typeOfUse: 'گرید خوراکی',
+                country: 'چین',
+                lable: 'Zibo',
+            },
+        ],
+        inPage: [
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+
+        ]
+    },
+    {
+        id: 5,
+        data: [
+            "#254",
+            {
+                img_Cover: '/ProductsImages/inPage/01.png',
+                img_InPage: '/ProductsImages/inPage/01.png',
+                name_Persian: 'آسکوربیک اسید (ویتامین C)',
+                name_English: 'Ascorbic Acid',
+                category: 'اسید',
+                weight: 25,
+                url: '/images/avatar.png',
+                packing: 25,
+                typeOfUse: 'گرید خوراکی',
+                country: 'چین',
+                lable: 'Zibo',
+            },
+        ],
+        inPage: [
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+            {
+                title: '',
+                text: '',
+            },
+
+        ]
+    },
+
+
+    // Add more products
+];
+export default Products;
---
+++ src/pages/Products/[id].js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import products from './ProductList'
+import Header from '@/Components/Header';
+import { Box, Button, Grid, List, ListItem, SvgIcon, Typography } from '@mui/material';
+const ProductDetails = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    const product = products.find((p) => String(p.id) === id);
+    // const product = products.map((p) => p);
+
+    if (!product) {
+        return <div>Loading...</div>;
+    }
+
+
+    return (
+        <>
+            <Header />
+            <Grid container sx={{
+                backgroundImage: `url('/bg2.png')`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '16px',
+                // height: '100vh',
+                flexDirection: 'column'
+            }}>
+                <Grid container sx={{ display: 'flex', flexDirection: 'row', direction: 'rtl' }} id='#top'>
+                    <Grid xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Box
+                            component={'img'}
+                            sx={{ width: { xs: '100%', sm: '90%' } }}
+                            src={product.data[1].img_InPage}
+                        />
+                    </Grid>
+                    <Grid xs={12} md={6} container sx={{ display: 'flex', alignContent: 'center' }}>
+                        <Grid item md={12}>
+                            <Typography variant='body2' sx={{ fontSize: '23px', fontWeight: 200, color: '#727490' }}>محصولات / {product.data[1].category}</Typography>
+                            <Typography variant='h1' sx={{ fontSize: '38px', fontWeight: 800, color: '#1F2241', my: '15px' }}>{product.data[1].name_Persian}</Typography>
+                            <Typography variant='body2' sx={{ fontSize: '23px', fontWeight: 400, color: '#727490' }}> {product.data[1].name_English}</Typography>
+                        </Grid>
+                        <Grid item md={12} mt={5}>
+                            <Typography variant='body2' sx={{ fontSize: '38px', fontWeight: 800, color: '#1F2241', my: '15px' }}>ویژگی ها</Typography>
+                            <Grid>
+                                <List sx={{ listStyleType: 'disc', direction: 'rtl' }}>
+                                    <ListItem sx={{ display: 'list-item', direction: 'rtl', textAlign: 'right' }}>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 400, color: '#727490', my: '15px', }}>
+                                            دسته بندی:
+                                        </Typography>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 600, color: '#1F2241', my: '15px', }}>
+                                            {product.data[1].category}
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item', direction: 'rtl', textAlign: 'right' }}>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 400, color: '#727490', my: '15px', }}>
+                                            گرید:
+                                        </Typography>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 600, color: '#1F2241', my: '15px', }}>
+                                            {product.data[1].typeOfUse}
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item', direction: 'rtl', textAlign: 'right' }}>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 400, color: '#727490', my: '15px', }}>
+                                            بسته بندی:
+                                        </Typography>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 600, color: '#1F2241', my: '15px', }}>
+                                            بسته
+                                            {product.data[1].weight}
+                                            کیلوگرمی
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item', direction: 'rtl', textAlign: 'right' }}>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 400, color: '#727490', my: '15px', }}>
+                                            کشور تولید کننده:
+                                        </Typography>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 600, color: '#1F2241', my: '15px', }}>
+                                            {product.data[1].country}
+                                        </Typography>
+                                    </ListItem>
+                                    <ListItem sx={{ display: 'list-item', direction: 'rtl', textAlign: 'right' }}>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 400, color: '#727490', my: '15px', }}>
+                                            برند محصول:
+                                        </Typography>
+                                        <Typography variant='span' sx={{ fontSize: '24px', fontWeight: 600, color: '#1F2241', my: '15px', }}>
+                                            {product.data[1].lable}
+                                        </Typography>
+                                    </ListItem>
+                                </List>
+                                <Grid sx={{ mt: '35px' }}>
+                                    <Button variant='h4' href='#down'
+                                        sx={{ fontSize: { xs: '100%', md: '23px' }, width: '90%', color: '#1F2241', backgroundColor: '#1F22411A', borderRadius: '15px', p: '15px', m: 'auto', display: 'flex', flexDirection: { xs: 'column', md: 'row' }, alignItems: 'center', justifyContent: 'center', fontWeight: { xs: 800, md: 600 } }}
+                                    >
+                                        کربوکسی متیل سلولز (CMC) و کاربرد های آن را بیشتر بشناسید
+
+                                        <SvgIcon>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="29" height="28" viewBox="0 0 29 28" fill="none">
+                                                <path d="M23.5988 10.496L16.1083 17.9866C15.2236 18.8712 13.7761 18.8712 12.8915 17.9866L5.40088 10.496" stroke="#1F2241" stroke-width="3" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+                                            </svg>
+                                        </SvgIcon>
+                                    </Button>
+                                </Grid>
+
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid container id='#down'>
+                        <Grid md={12}>
+
+                        </Grid>
+
+                    </Grid>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export default ProductDetails;
